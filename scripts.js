00c40a891b313aab2092fa663fea0cc01062b56b
Dreividsantos <deivid.santos7@outlook.com>
2024-09-14 10:48:14 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,9 @@
+document.getElementById('passwordForm').onsubmit = function(e) {
+    e.preventDefault();
+    const site = document.getElementById('site').value;
+    const password = document.getElementById('password').value;
+    const row = `<tr><td>${site}</td><td>${password}</td></tr>`;
+    document.getElementById('passwordTable').innerHTML += row;
+    this.reset();  // Limpa o formulário
+};
+
